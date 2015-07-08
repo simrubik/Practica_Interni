6c8c9497d326c33aa2809b00529ccca9a156f65e
a.borosu <a.borosu@levi9.com>
2015-07-08 15:43:49 +0300

    commit daniel

--- Ex2/src/main/java/com/ana/NewClass2.java
+++ Ex2/src/main/java/com/ana/NewClass2.java
@@ -4,6 +4,7 @@ public class NewClass2 {
 
 	public static void main(String[] args) {
 		System.out.println("Am creat o clasa.");
+		System.out.println("Am modificat clasa din daniel branch");
 
 	}
 
