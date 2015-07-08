2153560e0fe8fcdf763c639eaf470a00365bf943
a.borosu <a.borosu@levi9.com>
2015-07-08 15:45:05 +0300

    commit ana

--- Ex2/src/main/java/com/ana/NewClass2.java
+++ Ex2/src/main/java/com/ana/NewClass2.java
@@ -4,7 +4,7 @@ public class NewClass2 {
 
 	public static void main(String[] args) {
 		System.out.println("Am creat o clasa.");
-
+		System.out.println("Modificat din ana pe acelasi rand");
 	}
 
 }
